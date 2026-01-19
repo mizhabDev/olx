a4f0b04ed84e1ee5ea92dd621eab5a5ba71025ee
VICTUS <user@example.com>
2026-01-19 09:54:04 +0530

    send email configuration

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -5,22 +5,31 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.APP_EMAIL,
-      pass: process.env.APP_PASSWORD,
-    },
-  });
+  try {
 
-  const info = await transporter.sendMail({
-    from: `"olx" <${process.env.APP_EMAIL}>`,
-    to,
-    subject,
-    html,
-  });
 
-  console.log("Email sent successfully ✅:", info.messageId);
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.APP_EMAIL,
+        pass: process.env.APP_PASSWORD,
+      },
+      connectionTimeout: 20000,
+    });
+
+    const info = await transporter.sendMail({
+      from: `"olx" <${process.env.APP_EMAIL}>`,
+      to,
+      subject,
+      html,
+    });
+
+    console.log("Email sent successfully ✅:", info.messageId);
+  } catch (error) {
+    console.log("Email sent failed ❌:", error);
+  }
 };
 
 
