0c25b607041f15f92396ea084eee4ec2a01b5c1b
VICTUS <user@example.com>
2026-01-19 12:13:13 +0530

    feat: add email sending utility using Nodemailer for sending emails via Gmail.

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,23 +1,27 @@
-import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
-export const sendEmail = async (
-  to: string,
-  subject: string,
-  html: string
-) => {
-  try {
-    await resend.emails.send({
-      from: "Sellzy <onboarding@resend.dev>", // works immediately
-      to,
-      subject,
-      html,
-    });
+dotenv.config();
 
-    console.log("Email sent successfully ✅");
-  } catch (error) {
-    console.error("Email sending failed ❌:", error);
-    throw error; // IMPORTANT
-  }
+export const sendEmail = async (to: string, subject: string, html: string) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.APP_EMAIL,
+      pass: process.env.APP_PASSWORD,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: `"olx" <${process.env.APP_EMAIL}>`,
+    to,
+    subject,
+    html,
+  });
+
+  console.log("Email sent successfully ✅:", info.messageId);
 };
+
+
+
