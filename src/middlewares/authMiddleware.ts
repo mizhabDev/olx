a440592f383af0fac067ba5cf384771d2f22f54c
VICTUS <user@example.com>
2026-01-21 15:39:43 +0530

    dubugging

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -8,6 +8,7 @@ import { AuthRequest } from "../types/auth";
 export const verifyToken = (req: AuthRequest, res: Response, next: NextFunction) => {
   try {
     const token =req.cookies?.token || req.headers.authorization?.split(" ")[1];
+    console.log("token", token);
     
 
     if (!token) {
